5ad06038a377199319b3c8e29319869fdd9dd884
SÜMEYRA <sumeyra_dvli@outlook.com>
2025-02-12 00:13:50 +0300

    firstcommit

--- src/test/java/pages/UserPages.java
+++ src/test/java/pages/UserPages.java
@@ -1,4 +1,11 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class UserPages {
+
+    public UserPages (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
