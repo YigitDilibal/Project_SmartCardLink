2c4366a24101259d9dee98474f3b72a0ec03acde
SÜMEYRA <sumeyra_dvli@outlook.com>
2025-02-12 00:12:17 +0300

    firstcommit

---
+++ src/test/java/pages/AdminPages.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AdminPages {
+
+    public AdminPages (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
