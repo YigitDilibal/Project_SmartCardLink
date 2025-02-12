4b48bed035e050ce9584b5166b4ad98e1b9d350f
YigitDilibal <archery10@outlook.com>
2025-02-12 21:45:36 +0200

    firstcommit

--- src/test/java/pages/AdminPages.java
+++ src/test/java/pages/AdminPages.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,4 +10,12 @@ public class AdminPages {
     public AdminPages (){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath = "(//span[text()='Allow all cookies'])[2]")
+    public WebElement cookiesKabulButonu;
+
+
+
+
+
 }
