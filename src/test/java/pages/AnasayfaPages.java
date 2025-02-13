bbc7beb993bb28f2f918816ca3e85ef0182c3cae
SÜMEYRA <sumeyra_dvli@outlook.com>
2025-02-13 16:30:53 +0300

    13.02.25

--- src/test/java/pages/AnasayfaPages.java
+++ src/test/java/pages/AnasayfaPages.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,4 +9,23 @@ public class AnasayfaPages {
     public AnasayfaPages(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(xpath = "//*[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")
+    public WebElement homepageSıgnInButonu;
+
+    @FindBy (id = "email")
+    public WebElement signInEmailKutusu;
+
+    @FindBy (id = "password")
+    public WebElement signInPasswordKutusu;
+
+    @FindBy (xpath = "//*[@class=\"btn btn-primary\"]")
+    public WebElement signInLoginButonu;
+
+    @FindBy (xpath = "//*[@class='bi bi-eye-slash-fill']")
+    public WebElement signInBieyeSlashFill;
+
+    @FindBy (id = "remember_me")
+    public WebElement signInRemembermeBox;
+
+
 }
