bf867245c8b4161d314e32fd2d74b2ba8565a457
SÜMEYRA <sumeyra_dvli@outlook.com>
2025-02-13 16:24:32 +0300

    13.02.25

--- src/test/java/pages/UserPages.java
+++ src/test/java/pages/UserPages.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,6 +10,25 @@ public class UserPages {
     public UserPages (){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    
-  
+
+    @FindBy(xpath = "//*[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']")
+    public WebElement homepageSıgnInButonu;
+
+    @FindBy (id = "email")
+    public WebElement signInEmailKutusu;
+
+    @FindBy (id = "password")
+    public WebElement signInPasswordKutusu;
+
+    @FindBy (xpath = "//*[@class=\"btn btn-primary\"]")
+    public WebElement signInLoginButonu;
+
+    @FindBy (xpath = "//*[@class='bi bi-eye-slash-fill']")
+    public WebElement signInBieyeSlashFill;
+
+    @FindBy (id = "remember_me")
+    public WebElement signInRemembermeBox;
+
+
+
 }
