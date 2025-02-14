dc7ee5adaed579c313c592ab5236f768bb69e75d
SÜMEYRA <sumeyra_dvli@outlook.com>
2025-02-14 12:59:58 +0300

    14.02.25

--- src/test/java/tests/SumeyraUS19US21US22.java
+++ src/test/java/tests/SumeyraUS19US21US22.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AdminPages;
@@ -8,6 +10,8 @@ import pages.AnasayfaPages;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import java.time.Duration;
 
 public class SumeyraUS19US21US22 {
 
@@ -24,7 +28,7 @@ public class SumeyraUS19US21US22 {
         anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminsumeyramail"));
         //Sıgn ın sayfasında password kutusunun oldugunu dogrular ve password kutusununa gecerli password girer.
         anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
-        //Login tusuna basar ve kendi sayfasına erişim saglar.
+        //Login tusuna basar ve kendi sayfasına erişim saglar.git
         anasayfaPages.signInLoginButonu.click();
         //Sıgn out olur ve sayfayı kapatır.
         adminPages.avatarDropdownMenuButonu.click();
@@ -44,14 +48,46 @@ public class SumeyraUS19US21US22 {
             anasayfaPages.signInEmailKutusu.sendKeys("computer@gmail.com");
             //Sıgn ın sayfasında password kutusunun oldugunu dogrular ve password kutusununa gecerli password girer.
             anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
-            //Login tusuna basar ve hata mesajı alır.
+            //Login tusuna basar.
             anasayfaPages.signInLoginButonu.click();
-            ReusableMethods.bekle(2);
-            String expectedHataMesajı = "These credentials do not match our records.";
-            String actualHataMesajı = Driver.getDriver().switchTo().alert().getText();
-            Assert.assertTrue(expectedHataMesajı.contains(actualHataMesajı));
+            // Başarılı bir şekilde login olamadığını test edin.
+            Assert.assertTrue(anasayfaPages.signInLoginButonu.isDisplayed());
            //sayfayı kapatır.
             Driver.quitDriver();
+    }
+    @Test
+    public void gecersizPasswordileLoginTesti () {
+        //Yönetici URL ile siteye erişir
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        //Yönetici Sıgn ın linkine tıklayın ve  Sıgn ın sayfasına erişim sağlayın
+        anasayfaPages.homepageSıgnInButonu.click();
+        //Sıgn ın sayfasında Email kutusunun oldugunu dogrular ve geçerli mail girer.
+        anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminsumeyramail"));
+        //Sıgn ın sayfasında password kutusunun oldugunu dogrular ve password kutusununa gecersiz password girer.
+        anasayfaPages.signInPasswordKutusu.sendKeys("Smart.1203");
+        //Login tusuna basarak login olmayı deneyin
+        anasayfaPages.signInLoginButonu.click();
+        // Başarılı bir şekilde login olamadığını test edin.
+        Assert.assertTrue(anasayfaPages.signInLoginButonu.isDisplayed());
+        //sayfayı kapatın.
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void manditoryFieldTest(){
+        //Yönetici URL ile siteye erişim sağlar
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        //Yönetici Sıgn ın linkini görüntüler ve Sıgn ın sayfasına erişim sağlar.
+        anasayfaPages.homepageSıgnInButonu.click();
+        //Sıgn ın sayfasında Email kutusunun boş bırakır.
+        anasayfaPages.signInEmailKutusu.sendKeys((""));
+        //Sıgn ın sayfasında password kutusunun boş bırakır.
+        anasayfaPages.signInPasswordKutusu.sendKeys("");
+        //Login tusuna basar ve login olamadığını doğrular.
+        anasayfaPages.signInLoginButonu.click();
+        Assert.assertTrue(anasayfaPages.signInLoginButonu.isDisplayed());
+        //Sayfayı kapatır.
+        Driver.quitDriver();
     }
 }
 
