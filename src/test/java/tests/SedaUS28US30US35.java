0a82006898b0190289e75cc2058c4939636fe6d6
Sedabudak <sedabudak92@gmail.com>
2025-02-19 00:05:59 +0300

    Seda-18.02.25

--- src/test/java/tests/SedaUS28US30US35.java
+++ src/test/java/tests/SedaUS28US30US35.java
@@ -1,5 +1,8 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.AdminPages;
 import pages.AnasayfaPages;
@@ -9,28 +12,79 @@ import utilities.Driver;
 
 public class SedaUS28US30US35 {
 
-    // US28 TC01
+    AnasayfaPages anasayfaPages;
+    AdminPages adminPages;
+    UserPages userPages;
 
-    AnasayfaPages anasayfaPages = new AnasayfaPages();
-    AdminPages adminPages = new AdminPages();
-    UserPages userPages = new UserPages();
-
-    @Test
-
-    public void BagliKullanicilarSekmesiTesti () {
-
-        Driver.getDriver().get(ConfigReader.getProperty("url"));
-        anasayfaPages.homepageSıgnInButonu.click();
-        anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminsedamail"));
-        anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
-        anasayfaPages.signInLoginButonu.click();
-
-        // Yukarıdaki adımlarla admin girişi yapılmış oldu ve dashboard sayfasına erişim sağlandı
-
-        adminPages.adminSekmesiSubscribedUserButonu.click(); // Abone kullanıcılar sekmesine erişildi
-
-
-
-
+    @BeforeMethod
+    public void setup() {
+        anasayfaPages = new AnasayfaPages();
+        adminPages = new AdminPages();
+        userPages = new UserPages();
     }
+
+        @Test // US28 TC01
+        public void AboneOlanKullanicilarSekmesiTesti () {
+
+
+            Driver.getDriver().get(ConfigReader.getProperty("url"));
+            anasayfaPages.homepageSıgnInButonu.click();
+
+
+            anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminSedaMail"));
+            anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
+            anasayfaPages.signInLoginButonu.click();
+
+
+            // Yukarıdaki adımlarla admin girişi yapılmış oldu ve dashboard sayfasına erişim sağlandı
+
+            adminPages.adminSekmesiSubscribedUserButonu.click(); // Abone kullanıcılar sekmesine erişildi
+
+        }
+
+        @Test // US30 TC01
+        public void BagliKullanicilarSekmesiTesti () {
+
+            Driver.getDriver().get(ConfigReader.getProperty("url"));
+            anasayfaPages.homepageSıgnInButonu.click();
+
+
+            anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminSedaMail"));
+            anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
+            anasayfaPages.signInLoginButonu.click();
+
+
+            // Yukarıdaki adımlarla admin girişi yapılmış oldu ve dashboard sayfasına erişim sağland
+
+            adminPages.adminSekmesiBagliKullanicilarButonu.click(); // Bağlı kullanıcılar sekmesine erişildi
+
+        }
+
+        @Test // US35 TC01
+        public void AdminGüvenliCikisIslemiTesti () {
+
+            Driver.getDriver().get(ConfigReader.getProperty("url"));
+            anasayfaPages.homepageSıgnInButonu.click();
+
+
+            anasayfaPages.signInEmailKutusu.sendKeys(ConfigReader.getProperty("adminSedaMail"));
+            anasayfaPages.signInPasswordKutusu.sendKeys(ConfigReader.getProperty("password"));
+            anasayfaPages.signInLoginButonu.click();
+
+
+            // Yukarıdaki adımlarla admin girişi yapılmış oldu ve dashboard sayfasına erişim sağland
+
+            userPages.avatarDropdownMenuButonu.click();
+            userPages.signOutButonu.click(); // Çıkış butonuna tıklanır
+            Assert.assertTrue(anasayfaPages.signInFormu.isDisplayed());
+            //Sayfayı kapatır.
+            Driver.quitDriver();
+
+
+
+
+        }
+
+
 }
+
