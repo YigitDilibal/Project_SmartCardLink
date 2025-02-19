c0df56ae84a98915a08992adb429cafba10cc97b
Burak-Keceli <burakkeceli@outlook.com>
2025-02-19 18:22:24 +0300

    Burak KECELİ UPDATE

--- src/test/java/pages/AdminPages.java
+++ src/test/java/pages/AdminPages.java
@@ -67,6 +67,164 @@ public class AdminPages {
     @FindBy (xpath = "(//*[@class='btn-close'])[5]")
     public WebElement withdrawalViewCloseButton;
 
+    ////////////////////////////////////////US29////////////////////////////////////////
+
+    @FindBy(xpath = "(//*[.='Plans'][1])")
+    public WebElement adminPlansSidebarButonu;
+
+    @FindBy(xpath = "//input[@class='form-control ps-8']")
+    public WebElement adminPlansTabloSearchInputAlani;
+
+    @FindBy(xpath = "(//span[@class='relative d-flex align-items-center'][1])")
+    public WebElement adminPlansTabloNameSiralamaButonu;
+
+    @FindBy(xpath = "(//span[@class='relative d-flex align-items-center'][2])")
+    public WebElement adminPlansTabloPriceSiralamaButonu;
+
+    @FindBy(xpath = "(//span[@class='relative d-flex align-items-center'][3])")
+    public WebElement adminPlansTabloStatusSiralamaButonu;
+
+    @FindBy(xpath = "(//span[@class='relative d-flex align-items-center'][4])")
+    public WebElement adminPlansTabloDurationSiralamaButonu;
+
+    @FindBy(xpath = "(//span[@class='relative d-flex align-items-center'][5])")
+    public WebElement adminPlansTabloMakeDefaultSiralamaButonu;
+
+    @FindBy(xpath = "(//a[.='Plans'][1])")
+    public WebElement adminPlansSayfasiBaslikYazisi;
+
+    @FindBy(xpath = "(//input[@name='status'][1])")
+    public WebElement adminPlansStatusToggleButonu;
+
+    @FindBy(xpath = "(//a[@title='Edit'][1])")
+    public WebElement adminPlansEditButonu;
+
+    @FindBy(xpath = "(//a[@title='Delete'][1])")
+    public WebElement adminPlansDeleteButonu;
+
+    @FindBy(id = "perPage")
+    public WebElement adminPlansShowDropdownButonu;
+
+    @FindBy(xpath = "//li[@class='page-item disabled']")
+    public WebElement adminPlansPageBackButonu;
+
+    @FindBy(xpath = "//button[.='›']")
+    public WebElement adminPlansPageNextButonu;
+
+    @FindBy(xpath = "(//a[.='New Plan'])[1]")
+    public WebElement adminPlansNewPlanButonu;
+
+    @FindBy(xpath = "//input[@name='name']")
+    public WebElement adminPlansNewPlanNameInputAlani;
+
+    @FindBy(xpath = "//span[@aria-labelledby='select2-frequency-container']")
+    public WebElement adminPlansFrequencyDropdownButonu;
+
+    @FindBy(xpath = "//span[@aria-labelledby='select2-currency_id-container']")
+    public WebElement adminPlansCurrencyDropdownButonu;
+
+    @FindBy(xpath = "//input[@placeholder='Enter Price']")
+    public WebElement adminPlansPriceInputAlani;
+
+    @FindBy(xpath = "//*[@id='no_of_vcards']")
+    public WebElement adminPlansKartNumarasiInputAlani;
+
+    @FindBy(xpath = "//*[@id='trial_days']")
+    public WebElement adminPlansTrialDaysInputAlani;
+
+    @FindBy(xpath = "//div[@class='col-lg-3 img-box mb-2'][2]")
+    public WebElement adminPlansTemplateSecmeAlani;
+
+    @FindBy(id = "featureAll")
+    public WebElement adminPlansSelectAllFeaturesCheckbox;
+
+    @FindBy(xpath = "//input[@name='products_services']")
+    public WebElement adminPlansFeatureSecmeAlani;
+
+    @FindBy(id = "planFormSubmit")
+    public WebElement adminPlansSaveButonu;
+
+    @FindBy(xpath = "//a[@class='btn btn-secondary']")
+    public WebElement adminPlansDiscardButonu;
+
+    @FindBy(xpath = "//h1[.='New Plan']")
+    public WebElement adminPlansNewPlanYazisi;
+
+    @FindBy(xpath = "//div[.='Select one or more Feature is required']")
+    public WebElement adminPlansFeatureSecmeyinceToastMesaji;
+
+    @FindBy(xpath = "//div[.='Multi templates is required']")
+    public WebElement adminPlansTemplateSecmeyinceToastMesaji;
+
+    @FindBy(xpath = "//input[@aria-controls='select2-frequency-results']")
+    public WebElement adminPlansFrequencySearchInputAlani;
+
+    @FindBy(xpath = "//ul[@id='select2-frequency-results']")
+    public WebElement adminPlansFrequencyNoResultsFoundText;
+
+    @FindBy(xpath = "//ul[@id='select2-currency_id-results']")
+    public WebElement adminPlansCurrencyNoResultsFoundText;
+
+    @FindBy(xpath = "//*[.='The name has already been taken.']")
+    public WebElement adminPlansAyniIsimHatasi;
+
+    @FindBy(xpath = "//*[.='Delete !']")
+    public WebElement adminPlansDeletePopupTitle;
+
+    @FindBy(xpath = "//*[.='Yes, Delete!']")
+    public WebElement adminPlansDeletePopupYesButonu;
+
+    @FindBy(xpath = "//*[.='No, Cancel']")
+    public WebElement adminPlansDeletePopupNoButonu;
+
+    @FindBy(xpath = "//*[.='Ok']")
+    public WebElement adminPlansDeletePopupOkButonu;
+
+    @FindBy(xpath = "//td[@class=\'text-center\']")
+    public WebElement adminPlansNoDataAvailableYazisi;
+
+    @FindBy(xpath = "(//td)[1]")
+    public WebElement adminPlansBurakCell;
+
+    @FindBy(xpath = "//input[@class='select2-search__field']")
+    public WebElement adminPlansCurrencyInputAlani;
+
+    @FindBy(xpath = "//li[@id='select2-currency_id-result-vcq6-1']")
+    public WebElement adminPlasnCurrencySecimi;
+
+
+    ////////////////////////////////////////US40////////////////////////////////////////
+
+    @FindBy(xpath = "//button[@id='dropdownMenuButton1']")
+    public WebElement menuButonu;
+
+    @FindBy(xpath = "//a[@id='changeLanguage']")
+    public WebElement changeLanguageButonu;
+
+    @FindBy(xpath = "//h3[.='Change Language']")
+    public WebElement changeLanguagePopupText;
+
+    @FindBy(xpath = "//*[@id='changeLanguageModal']/div/div/div/button")
+    public WebElement changeLanguagePopupXButonu;
+
+    @FindBy(xpath = "//span[@id='select2-selectLanguage-container']")
+    public WebElement changeLanguageDropdownButonu;
+
+    @FindBy(xpath = "//input[@class='select2-search__field']")
+    public WebElement changeLanguageInputAlani;
+
+    @FindBy(xpath = "//li[@id='select2-selectLanguage-result-068j-tr']")
+    public WebElement turkceDilSecimi;
+
+    @FindBy(xpath = "//button[@id='languageChangeBtn']")
+    public WebElement changeLanguagePopupKaydetButonu;
+
+    @FindBy(xpath = "//*[@id='changeLanguageForm']/div[2]/button[2]")
+    public WebElement changeLanguagePopupIptalButonu;
+
+    @FindBy(xpath = "//a[@class='dropdown-item text-gray-900 d-flex']")
+    public WebElement logoutButonu;
+
 
 
 }
