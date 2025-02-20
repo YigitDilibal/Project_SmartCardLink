4a1e43ce5af8624d7fb842d62118e31e1f257e8e
TugbaKaragulle <karagulletugba@gmail.com>
2025-02-20 15:39:39 +0100

    US02

--- src/test/java/tests/TugbaUS01US02.java
+++ src/test/java/tests/TugbaUS01US02.java
@@ -113,7 +113,7 @@ public class TugbaUS01US02 {
         String expectedUrl = "https://qa.smartcardlink.com/admin/dashboard";
         String actuallUrl = driver.getCurrentUrl();
 
-        Assert.assertEquals(expectedUrl,actuallUrl);
+        Assert.assertEquals(expectedUrl, actuallUrl);
 
         ReusableMethods.bekle(2);
 
@@ -162,14 +162,14 @@ public class TugbaUS01US02 {
         String expectedLink = "https://qa.smartcardlink.com/#frontContactUsTab";
         String actualLink = driver.getCurrentUrl();
 
-        Assert.assertEquals(expectedLink,actualLink);
+        Assert.assertEquals(expectedLink, actualLink);
 
         WebElement contactText = driver.findElement(By.xpath("//*[@class='heading text-success text-center margin-b-80px mb-5']"));
 
         String expectedText = "Contact Us";
         String actuallText = contactText.getText();
 
-        Assert.assertEquals(expectedText,actuallText);
+        Assert.assertEquals(expectedText, actuallText);
 
         ReusableMethods.bekle(3);
 
@@ -220,14 +220,14 @@ public class TugbaUS01US02 {
         String expectedLink = "https://qa.smartcardlink.com/#frontPricingTab";
         String actualLink = driver.getCurrentUrl();
 
-        Assert.assertEquals(expectedLink,actualLink);
+        Assert.assertEquals(expectedLink, actualLink);
         ReusableMethods.bekle(2);
 
         String expectedText = "Choose a plan that's right for you";
 
         String actualText = driver.findElement(By.xpath("(//h2[@class='text-dark text-center mb-60'])[2]")).getText();
 
-        Assert.assertEquals(expectedText,actualText);
+        Assert.assertEquals(expectedText, actualText);
         ReusableMethods.bekle(2);
 
     }
@@ -287,4 +287,90 @@ public class TugbaUS01US02 {
         ReusableMethods.bekle(2);
 
     }
+
+    //User story 02 kodlari
+
+    @Test
+    public void test06() {   //Sifre girerek kullanicinin sayfaya girisi
+
+        driver.get("https://qa.smartcardlink.com/");
+
+        WebElement signInButonu = driver.findElement(By.xpath("//*[@class='btn btn-white fs-18 ms-3 d-lg-block d-none']"));
+
+        Assert.assertTrue(signInButonu.isDisplayed());
+        ReusableMethods.bekle(2);
+
+        signInButonu.click();
+        ReusableMethods.bekle(2);
+
+        String expectedUrl = "https://qa.smartcardlink.com/login";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl, actualUrl);
+
+        ReusableMethods.bekle(2);
+
+        String actualText = driver.findElement(By.xpath("//*[@*='text-center mb-7']")).getText();
+        String expectedText = "Sign In";
+
+        Assert.assertTrue(actualText.contains(expectedText));
+
+        ReusableMethods.bekle(2);
+    }
+
+    @Test
+    public void test07() {   //Sifre girerek kullanicinin sayfaya girisi
+
+        driver.get("https://qa.smartcardlink.com/");
+
+        WebElement features = driver.findElement(By.xpath("(//*[@class='nav-link mt-1'])[1]"));
+
+        Assert.assertTrue(features.isDisplayed());
+        ReusableMethods.bekle(2);
+
+        features.click();
+        ReusableMethods.bekle(2);
+
+        String expectedUrl = "https://qa.smartcardlink.com/#frontFeaturesTab";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl, actualUrl);
+
+        ReusableMethods.bekle(2);
+
+        String actualText = driver.findElement(By.xpath("(//h2[@class='text-dark text-center mb-60'])[1]")).getText();
+        String expectedText = "Features";
+
+        Assert.assertTrue(actualText.contains(expectedText));
+
+        ReusableMethods.bekle(2);
+    }
+
+    @Test
+    public void test08() {   //Sifre girerek kullanicinin sayfaya girisi
+
+        driver.get("https://qa.smartcardlink.com/");
+
+        WebElement logo = driver.findElement(By.xpath("//*[@class='img-fluid navbar-logo']"));
+        Actions actions = new Actions(driver);
+        ReusableMethods.bekle(2);
+        actions.doubleClick().perform();
+        ReusableMethods.bekle(3);
+        WebElement subscribe = driver.findElement(By.xpath("//*[@class='text-dark text-center mb-3']"));
+
+        Assert.assertTrue(subscribe.isDisplayed());
+        ReusableMethods.bekle(3);
+    }
+
+    @Test
+    public void test09() {   //Sifre girerek kullanicinin sayfaya girisi
+
+        driver.get("https://qa.smartcardlink.com/");
+
+        WebElement smartCardLinks = driver.findElement(By.xpath("//*[@class='text-dark mb-1']"));
+
+        Assert.assertTrue(smartCardLinks.isDisplayed());
+        ReusableMethods.bekle(3);
+    }
+
 }
